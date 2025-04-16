a9736c50840134ce441e8ed6ec0d87da9d1dc879
tzz1996 <1049713857@qq.com>
2025-04-16 21:40:59 +0800

    添加组播端口-报文对应关系xml解析过程，并根据结果添加组播监听

--- src/main/java/org/example/MulticastConfig.java
+++ src/main/java/org/example/MulticastConfig.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MulticastConfig {
     public static class MulticastGroup {
@@ -25,6 +34,19 @@ public class MulticastConfig {
         public String toString() {
             return group + ":" + port;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            MulticastGroup that = (MulticastGroup) o;
+            return port == that.port && group.equals(that.group);
+        }
+
+        @Override
+        public int hashCode() {
+            return 31 * group.hashCode() + port;
+        }
     }
 
     // 定义要监听的组播组列表
@@ -36,5 +58,75 @@ public class MulticastConfig {
         MULTICAST_GROUPS.add(new MulticastGroup("239.255.27.2", 8889));
         MULTICAST_GROUPS.add(new MulticastGroup("239.255.27.3", 8890));
         // 可以继续添加更多组播组
+    }
+
+    // XML配置类
+    @JacksonXmlRootElement(localName = "multicastConfig")
+    public static class MulticastXmlConfig {
+        @JacksonXmlProperty(localName = "group")
+        @JacksonXmlElementWrapper(useWrapping = false)
+        private List<GroupConfig> groups;
+
+        public List<GroupConfig> getGroups() {
+            return groups;
+        }
+
+        public void setGroups(List<GroupConfig> groups) {
+            this.groups = groups;
+        }
+    }
+
+    public static class GroupConfig {
+        @JacksonXmlProperty(isAttribute = true)
+        private String address;
+
+        @JacksonXmlProperty(isAttribute = true)
+        private int port;
+
+        @JacksonXmlProperty(localName = "message")
+        @JacksonXmlElementWrapper(useWrapping = false)
+        private List<String> messages;
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public void setPort(int port) {
+            this.port = port;
+        }
+
+        public List<String> getMessages() {
+            return messages;
+        }
+
+        public void setMessages(List<String> messages) {
+            this.messages = messages;
+        }
+    }
+
+    /**
+     * 解析XML配置文件，返回组播组和消息的映射
+     * @param xmlFilePath XML文件路径
+     * @return 组播组和消息列表的映射
+     * @throws IOException 如果解析失败
+     */
+    public static Map<MulticastGroup, List<String>> parseXmlConfig(String xmlFilePath) throws IOException {
+        XmlMapper xmlMapper = new XmlMapper();
+        MulticastXmlConfig config = xmlMapper.readValue(new File(xmlFilePath), MulticastXmlConfig.class);
+        
+        Map<MulticastGroup, List<String>> result = new HashMap<>();
+        for (GroupConfig groupConfig : config.getGroups()) {
+            MulticastGroup group = new MulticastGroup(groupConfig.getAddress(), groupConfig.getPort());
+            result.put(group, groupConfig.getMessages());
+        }
+        return result;
     }
 } 
--- src/main/java/org/example/MulticastReceiver.java
+++ src/main/java/org/example/MulticastReceiver.java
@@ -9,21 +9,23 @@ import io.netty.channel.socket.InternetProtocolFamily;
 import io.netty.channel.socket.nio.NioDatagramChannel;
 import io.netty.util.CharsetUtil;
 
+import java.io.File;
 import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 
 public class MulticastReceiver {
-    public static void main(String[] args) throws Exception {
+    private static void joinMulticastGroups(Map<MulticastConfig.MulticastGroup, List<String>> groupMessages) throws Exception {
         EventLoopGroup group = new NioEventLoopGroup();
         try {
             // 为每个组播组创建单独的channel
             List<Channel> channels = new ArrayList<>();
-            
+
             // 遍历所有组播组配置
-            for (MulticastConfig.MulticastGroup multicastGroup : MulticastConfig.MULTICAST_GROUPS) {
+            for (MulticastConfig.MulticastGroup multicastGroup : groupMessages.keySet()) {
                 Bootstrap bootstrap = new Bootstrap();
                 bootstrap.group(group)
                         .channelFactory(() -> new NioDatagramChannel(InternetProtocolFamily.IPv4))
@@ -45,7 +47,7 @@ public class MulticastReceiver {
 
                 Channel channel = bootstrap.bind(multicastGroup.getPort()).sync().channel();
                 channels.add(channel);
-                
+
                 // 获取所有网络接口并加入组播组
                 Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
                 while (interfaces.hasMoreElements()) {
@@ -53,23 +55,23 @@ public class MulticastReceiver {
                     try {
                         if (networkInterface.isUp() && !networkInterface.isLoopback()) {
                             InetSocketAddress groupAddress = new InetSocketAddress(
-                                multicastGroup.getGroup(), 
-                                multicastGroup.getPort()
+                                    multicastGroup.getGroup(),
+                                    multicastGroup.getPort()
                             );
                             ((NioDatagramChannel) channel).joinGroup(groupAddress, networkInterface).sync();
-                            System.out.println("Joined multicast group " + multicastGroup + 
+                            System.out.println("Joined multicast group " + multicastGroup +
                                     " on interface: " + networkInterface.getDisplayName());
                         }
                     } catch (Exception e) {
-                        System.err.println("Failed to join multicast group " + multicastGroup + 
-                                " on interface " + networkInterface.getDisplayName() + 
+                        System.err.println("Failed to join multicast group " + multicastGroup +
+                                " on interface " + networkInterface.getDisplayName() +
                                 ": " + e.getMessage());
                     }
                 }
             }
-            
+
             System.out.println("Multicast receiver started. Waiting for messages...");
-            
+
             // 等待所有channel关闭
             for (Channel channel : channels) {
                 channel.closeFuture().await();
@@ -77,5 +79,20 @@ public class MulticastReceiver {
         } finally {
             group.shutdownGracefully();
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // 解析XML配置文件
+        String configPath = new File("multicast-config.xml").getAbsolutePath();
+        Map<MulticastConfig.MulticastGroup, List<String>> groupMessages = MulticastConfig.parseXmlConfig(configPath);
+        
+        // 打印配置信息
+        System.out.println("Loaded multicast configuration:");
+        for (Map.Entry<MulticastConfig.MulticastGroup, List<String>> entry : groupMessages.entrySet()) {
+            System.out.println("Group: " + entry.getKey());
+            System.out.println("Messages: " + entry.getValue());
+        }
+
+        joinMulticastGroups(groupMessages);
     }
 } 
